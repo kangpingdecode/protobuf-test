4a55331139758b7e8eaece5eb2cdfeefc209849b
kangpingdecode <15558012187@163.com>
2020-06-23 15:05:05 +0800

    protobuf

---
+++ src/main/java/com/kangping/protobuf/Test.java
@@ -0,0 +1,16 @@
+package com.kangping.protobuf;
+
+/**
+ * <p>
+ * 功能：
+ * </p>
+ *
+ * @author kangping
+ * Copyright Inc. All rights reserved
+ * @version v1.0
+ * @ClassName: Test
+ * @date 2020/6/23
+ */
+
+public class Test {
+}
